f49195cc403efc5718d41f977254f5ab26078596
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2022-11-08 14:12:43 +0200

    rollback to 'live' version for educational purposes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,104 +1,146 @@
 <script lang="ts">
-	import { RESET_INTERVAL } from '$lib/config';
+	import HelpCircle from '$lib/icons/HelpCircle.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	export const prerender = true;
+	let showOriginal = false;
+
+	const resetPic = async () => {
+		const response = await fetch(`/api/reset?secret=${data.secret}`);
+
+		if (response.ok) {
+			location.reload();
+		} else {
+			alert('something went wrong');
+		}
+	};
 </script>
 
 <svelte:head>
-	<title>About</title>
+	<title>You are visit #{data.count}</title>
+	<link rel="icon" href={'data: image/jpeg; base64,' + data.pic} />
 </svelte:head>
 
-<main>
-	<h1>About</h1>
+<img
+	src={showOriginal ? '/1.jpg' : 'data: image/jpeg; base64,' + data.pic}
+	width={1920}
+	height={1281}
+	alt="image of a landscape compressed {data.count} times"
+/>
 
-	<p>
-		<b>
-			I had to shut the page down due to rapidly rising database costs, I wonder what could have
-			brought that... ;) Thanks for visiting!
-		</b>
+<nav>
+	<a class="about" href="/about"><HelpCircle /> About</a>
+
+	<div class="buttons">
+		{#if data.secret}
+			<button on:click={resetPic}>Reset this picture for everyone</button>
+		{/if}
+
+		<button on:click={() => (showOriginal = !showOriginal)}>
+			Show {showOriginal ? 'current' : 'original'}
+		</button>
+	</div>
+
+	<p class="credits">
+		Photo by
+		<a
+			href="https://unsplash.com/@baileyzindel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+		>
+			Bailey Zindel
+		</a>
+		on
+		<a
+			href="https://unsplash.com/s/photos/landscape?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+			>Unsplash</a
+		>
 	</p>
-
-	<p>
-		This website was a small experiment, where each visit to the page would deteriorate the main
-		image. Every {RESET_INTERVAL} visitors a reset button appeared on the bottom, which could be used
-		to restore the image back to its original quality.
-		<br />
-		<br />
-		You could see this website as a metaphor for the effects of tourism, how humanity affects nature,
-		or how meme quality degrades over time as they are shared.
-		<br />
-		<br />
-		The source code is available on
-		<a href="https://github.com/kumpmati/jpeg" target="_blank" rel="noreferrer">GitHub</a>.
-	</p>
-
-	<h2>Reset history</h2>
-	<ul class="history">
-		{#each data.pics.sort((a, b) => b.date.getTime() - a.date.getTime()) as pic}
-			<li class="history-item">
-				<h3>{pic.date.toLocaleString()}</h3>
-				<p>Final count before reset: <b>{pic.count}</b></p>
-				<img
-					src={'data: image/jpeg; base64,' + pic.image}
-					alt="state of the picture on {pic.date}"
-				/>
-			</li>
-		{/each}
-
-		<li class="history-item">
-			<h3>Original image</h3>
-			<p class="credits">
-				Photo by
-				<a
-					href="https://unsplash.com/@baileyzindel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-				>
-					Bailey Zindel
-				</a>
-				on
-				<a
-					href="https://unsplash.com/s/photos/landscape?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-					>Unsplash</a
-				>
-			</p>
-			<img src={'/1.jpg'} alt="landscape" />
-		</li>
-	</ul>
-</main>
+</nav>
 
 <style>
-	.history {
-		display: flex;
-		gap: 2rem;
-		flex-direction: column;
-		margin: 0;
-		padding: 0;
-		list-style: none;
-	}
-
-	.history-item h3,
-	.history-item p {
-		margin: 0.25rem 0;
-	}
-
 	img {
+		position: absolute;
+		top: 0;
+		left: 0;
+		object-fit: cover;
 		width: 100%;
-		border-radius: 0.5rem;
+		height: 100%;
 	}
 
-	main {
-		margin: 0 auto;
-		margin-top: 10rem;
-		margin-bottom: 5rem;
-		max-width: 30rem;
-		padding: 0 1rem;
+	.buttons {
+		position: absolute;
+		left: 50%;
+		bottom: 0.5rem;
+		transform: translateX(-50%);
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 0.5rem;
 	}
 
-	p {
-		font-family: Arial, Helvetica, sans-serif;
-		color: rgb(212, 212, 212);
-		line-height: 1.5;
+	button {
+		font-family: 'Urbanist';
+		z-index: 100;
+		border: none;
+		color: rgb(0, 0, 0);
+		background: rgba(255, 255, 255, 0.7);
+		backdrop-filter: blur(10px);
+		font-size: 1rem;
+		font-weight: bold;
+		padding: 0.5rem 2rem;
+		border-radius: 0.75rem;
+		cursor: pointer;
+		width: fit-content;
+		transform: scale(1);
+		transition: transform 200ms;
+	}
+
+	button:hover {
+		transform: scale(1.05);
+	}
+
+	nav {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		padding: 0.5rem;
+		width: 100%;
+		z-index: 100;
+		color: black;
+	}
+
+	nav a {
+		display: inline-flex;
+		color: black;
+	}
+
+	.credits {
+		font-size: 12px;
+		margin: 0;
+		background-color: rgba(255, 255, 255, 0.7);
+		backdrop-filter: blur(10px);
+		padding: 0.25rem 0.75rem;
+		border-radius: 3rem;
+	}
+
+	.about {
+		display: flex;
+		align-items: center;
+		background: rgba(255, 255, 255, 0.7);
+		backdrop-filter: blur(10px);
+		padding: 0.2rem;
+		padding-right: 0.75rem;
+		border-radius: 5rem;
+		text-decoration: none;
+		font-weight: bold;
+		gap: 0.5rem;
+		transition: transform 200ms;
+	}
+
+	.about:hover {
+		transform: scale(1.05);
 	}
 </style>
